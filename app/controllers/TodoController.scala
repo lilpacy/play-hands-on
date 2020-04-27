012a3bd4b50c1e20b2020a570d9306c12bee59d1
lilpacy <revivedtomorrow@gmail.com>
2020-04-27 19:22:22 +0900

    登録フォームview作成

--- app/controllers/TodoController.scala
+++ app/controllers/TodoController.scala
@@ -6,13 +6,28 @@ import play.api.data._
 import play.api.data.Forms._
 import services._
 
-class TodoController @Inject()(mcc: MessagesControllerComponents)
+class TodoController @Inject()(todoService: TodoService,
+                               mcc: MessagesControllerComponents)
     extends MessagesAbstractController(mcc) {
+
   def helloworld() = Action { implicit request: MessagesRequest[AnyContent] =>
     Ok("Hello World")
   }
+
   def list() = Action { implicit request: MessagesRequest[AnyContent] =>
-    val items: Seq[Todo] = Seq(Todo("Todo1"), Todo("Todo2"))
+    val items: Seq[Todo] = todoService.list()
     Ok(views.html.list(items))
+  }
+
+  val todoForm: Form[String] = Form("name" -> nonEmptyText)
+
+  def todoNew() = Action { implicit request: MessagesRequest[AnyContent] =>
+    Ok(views.html.createForm(todoForm))
+  }
+
+  def todoAdd() = Action { implicit request: MessagesRequest[AnyContent] =>
+    val name: String = todoForm.bindFromRequest().get
+    println(name)
+    Ok("Save")
   }
 }
